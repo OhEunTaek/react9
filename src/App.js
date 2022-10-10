0b0fe391189f7c8a2a0974df13cd895ccd708456
OhEunTaek <giloet777@gmail.com>
2022-10-10 18:17:51 +0900

    stpe2 sass환경 추가 - 주석시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 
-import './App.css';
+import './css/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <figure>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </figure>
   );
 }
 
 export default App;
+
+
+/*
+메모장 이후의 내용시작
+
+이렇게 style을 입히면 개발자창에서 엘리멘츠를 보면 head부분에 style로 넣어지게 되는것을 확인
+태그 안에 자동완성기능을 하고싶지만 기존처럼 되지 않는다
+
+세팅에서 
+"emmet.includeLanguages": {
+        "javascript": "javascriptreact"
+     }, 을 적용시켜야 한다
+
+이후 자동완성기능을 한번 사용해본다
+*/
